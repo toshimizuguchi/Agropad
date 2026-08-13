b0cf9b20dfaf02003de88beddc8269d2ffccd50c
Vinicius Toshi Mizuguchi <toshi0123.mizuguchi@gmail.com>
2026-08-13 11:42:42 -0300

    fix: config CORS

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
-from backend.models.clientes import Cliente
-from backend.models.pedidos import Pedido
-from backend.models.itens_pedidos import ItemPedido
+from fastapi.middleware.cors import CORSMiddleware
+
 from backend.routes.clientes import router as clientes_router
 from backend.routes.pedidos import router as pedidos_router
 from backend.routes.itens_pedidos import router as itens_pedidos_router
+
 app = FastAPI()
+app.add_middleware(
+CORSMiddleware,
+allow_origins=[
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "https://agropad-two.vercel.app/"
+],
+allow_credentials=True,
+allow_methods=["*"],
+allow_headers=["*"],
+)
 
 app.include_router(clientes_router)
 app.include_router(pedidos_router)
