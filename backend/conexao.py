647dd76d12d1eb7157ba79fc71222e9f01d4b971
Vinicius Toshi Mizuguchi <toshi0123.mizuguchi@gmail.com>
2026-08-19 23:16:10 -0300

    Add: Security fix

--- backend/conexao.py
+++ backend/conexao.py
@@ -6,7 +6,7 @@ from supabase import create_client, Client
 load_dotenv()
 url= os.getenv("SUPABASE_URL")
 key=os.getenv("SUPABASE_KEY")
-
+api_key = os.getenv("API_KEY")
 
 options = ClientOptions(
     postgrest_client_timeout=30,  # Tempo limite em segundos
